8cf7ab0eabf9ac8bbc47936c152a6905fad22858
Piero <ironpiero1@gmail.com>
2026-04-30 15:46:15 -0300

    Trabajo con clases en python y tipo de datos

--- app3.py
+++ app3.py
@@ -14,13 +14,14 @@ class Persona:
 
     #metodos 
     
-    def  __init__(self,nombre, apellido, edad, sexo,altura, peso):
+    def  __init__(self,nombre, apellido, edad, sexo,altura, peso,fecha_nac):
         self.nombre=nombre
         self.apellido=apellido
         self.edad=edad
         self.sexo=sexo
-        self.altura
+        self.altura =altura
         self.peso=peso
+        self.fecha_nac=self.fecha_nac
 
     def imprimir_datos(self): 
         print("--------------------DATOS DE LA PERSONA ---------------------------")
@@ -28,12 +29,19 @@ class Persona:
         print("Apellido",self.apellido)
         print("Edad",self.edad)
         print("Sexo",self.sexo)
-        print("")
+        print("--------------------------------------------------------------------")
 
 
 
 nombre =str(input("Ingrese su nombre:"))
 apellido=str(input("Ingrese su apellido:"))
 edad=int(input("Ingrese su edad:"))
+sexo=str(input("Ingrese el sexo (M-F):"))
+altura=float(input("Ingrese la altura en metros:"))
+peso=float(input("Ingrese el peso:"))
+fecha_nac =date(input("Ingrese la fecha de nacimiento"))
+
+persona =Persona(nombre,apellido,edad,sexo,altura,peso,fecha_nac)
+persona.imprimir_datos(persona)
 
             
